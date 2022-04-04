f5aca9b9d46eee4526940f8716ea73651a94f043
Caeleb Gensler <cgensler49@gmail.com>
2022-04-03 22:22:18 -0500

    finished activity 7

---
+++ activity-7/js/scripts.js
@@ -0,0 +1,64 @@
+var tasks = [];
+
+var taskStatus = {
+  active: 'active',
+  completed: 'completed'
+};
+
+function Task (id, name, status) {
+  this.id = id;
+  this.name = name;
+  this.status = status;
+}
+
+function addTaskElement (task) {
+  var listEl = document.getElementById('active-list');
+  var taskEl = document.createElement('li');
+  var textEl = document.createTextNode(task.name);
+
+  taskEl.setAttribute('id', task.id);
+  taskEl.appendChild(textEl);
+  listEl.appendChild(taskEl);
+}
+
+function addTask(e) {
+  var inputEl = document.getElementsById('input-task');
+  if(inputEl.value != '') {
+    var id = 'item-' + tasks.length;
+    var task = new Task(id, inputEl.value, taskStatus.active);
+    tasks.push(task);
+
+    addTaskElement(task);
+
+    inputEl.value = '';
+  }
+}
+
+function completeTask (e) {
+  var taskEl = e.target;
+  var id = taskEl.id;
+
+  for(var i = 0; i < tasks.length; i++){
+    if (tasks[i],id === id) {
+      tasks[i].status = taskStatus.completed;
+      break;
+    }
+  }
+
+  taskEl.remove();
+  document.getElementById('completed-list').appendChild(taskEl);
+}
+
+function clickButton (e) {
+  if (e.keyCode === 13) {
+    document.getElementById('add-task').click();
+  }
+}
+
+function init () {
+  document.getElementById('add-task').onclick = addTask;
+  document.getElementById('active-list').onclick = completeTask;
+  document.getElementById('input-task').onkeypress = clickButton;
+}
+
+init();
